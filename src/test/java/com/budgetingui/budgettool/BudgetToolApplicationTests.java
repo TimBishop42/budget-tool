5b7dc0fb1946af1296200005737c87a539ab450f
unknown <tim.bishop@tsgforce.com>
2019-10-19 11:25:08 +1100

    Initial Commit

---
+++ src/test/java/com/budgetingui/budgettool/BudgetToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgetingui.budgettool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
