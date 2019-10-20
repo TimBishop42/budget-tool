bbe5490f1d417b51be600f402f82f2facf8a4c49
bishoptim453@gmail.com <tim.bishop@tsgforce.com>
2019-10-20 18:59:12 +1100

    UI tinkering

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    isLoading: true,
+    purchases: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/tool/api/getTransactions');
+    const body = await response.json();
+    this.setState({ purchases: body, isLoading: false });
+  }
+
+  render() {
+    const {purchases, isLoading} = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="App-intro">
+            <h2>Test Transactions</h2>
+            <div >
+                hello
+              </div>
+            {/* {purchases.map(purchase =>
+              <div key={purchase.id}>
+                {purchase.name}
+              </div>
+            )} */}
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
