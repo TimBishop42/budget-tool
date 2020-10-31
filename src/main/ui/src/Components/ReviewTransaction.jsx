a253e712d7b26d10323c381c2892ab45a98fec0f
Tim Bishop <bishoptim453@gmail.com>
2020-10-31 14:57:50 +1100

    change review transaction to table format

--- src/main/ui/src/Components/ReviewTransaction.jsx
+++ src/main/ui/src/Components/ReviewTransaction.jsx
@@ -1,6 +1,26 @@
 import React from "react";
 import TransactionService from "../Rest/TransactionService";
 import Grid from "@material-ui/core/Grid";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+    },
+});
+// const classes = useStyles();
+
+// export default function BasicTable() {
+//     const classes = useStyles();
+// }
 
 
 class ReviewTransaction extends React.Component {
@@ -18,27 +38,12 @@ class ReviewTransaction extends React.Component {
     async componentDidMount() {
         console.log("getting transaction list")
         TransactionService.reviewTransactions()
-                .catch((error) => {
-                    console.log(error);
-                })
-                .then(response => response.data)
-                .then(data => this.setState({ transactions: data }));
-                }
-
-    
-
-    // async getPurchases() {
-    //     TransactionService.reviewTransactions()
-    //         .catch((error) => {
-    //             console.log(error);
-
-
-    //         })
-    //         .then((response) => {
-    //             console.log("api response: ", response)
-    //             return response
-    //         });
-    // }
+            .catch((error) => {
+                console.log(error);
+            })
+            .then(response => response.data)
+            .then(data => this.setState({ transactions: data }));
+    }
 
 
 
@@ -46,25 +51,32 @@ class ReviewTransaction extends React.Component {
         return (
             <div className="transactins-review">
                 <h2>Review Transactions</h2>
+                <TableContainer component={Paper}>
+                    <Table  size="small" aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="right">Category</TableCell>
+                                <TableCell align="right">Description</TableCell>
+                                <TableCell align="right">Name</TableCell>
+                                <TableCell align="right">Cost</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
 
-                {this.state.transactions.map(transaction =>
-                    <div key={transaction.id}>
-                        <Grid container direction="row" spacing={2}>
-                            <Grid item>
-                                {transaction.category}
-                            </Grid>
-                            <Grid item>
-                                {transaction.description}
-                            </Grid>
-                            <Grid item>
-                                {transaction.name}
-                            </Grid>
-                            <Grid item>
-                                {transaction.cost}
-                            </Grid>
-                        </Grid>
-                    </div>
-                )}
+                            {this.state.transactions.map(transaction =>
+                                <TableRow key={transaction.name}>
+                                    {/* <TableCell component="th" scope="row">
+                                        {transaction.name}
+                                    </TableCell> */}
+                                    <TableCell align="right">{transaction.category}</TableCell>
+                                    <TableCell align="right">{transaction.description}</TableCell>
+                                    <TableCell align="right">{transaction.name}</TableCell>
+                                    <TableCell align="right">{transaction.cost}</TableCell>
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
 
             </div>
 
